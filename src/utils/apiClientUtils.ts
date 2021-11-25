8a4a1a6010090ebc02886ec1d49da3b44a818d52
kinokoruumu <kinokoruumu@gmail.com>
2021-11-25 23:20:06 +0900

    add api

---
+++ src/utils/apiClientUtils.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance } from "axios"
+import applyConverters from "axios-case-converter"
+
+export type APIClient = AxiosInstance
+
+export function createAPIClient(): APIClient {
+  const instance = axios.create({
+    baseURL: process.env.API_ORIGIN,
+    timeout: 20000,
+  })
+
+  instance.interceptors.request.use((request) => {
+    const headers = request.headers != null ? request.headers : {}
+
+    request.headers = headers
+    return request
+  })
+
+  return applyConverters(instance)
+}
+
+export const apiClient = createAPIClient()
